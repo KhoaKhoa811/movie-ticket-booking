b482b898efba4f89c9bee30b877d6bf78e680665
KhoaKhoa811 <nguyenvoanhkhoa9487@gmail.com>
2025-03-18 11:26:43 +0700

    adding cinema entities part

---
+++ src/main/java/com/example/movieticketbooking/entity/CinemaEntity.java
@@ -0,0 +1,37 @@
+package com.example.movieticketbooking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@Entity
+@Table(name = "cinema")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CinemaEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(nullable = false)
+    private String name;
+    @Column(nullable = false)
+    private String address;
+    @Column(nullable = false)
+    private String street;
+    @Column(nullable = false)
+    private String ward;
+    @Column(nullable = false)
+    private String district;
+    @Column(nullable = false)
+    private String city;
+    @Column(nullable = false)
+    private String phoneNumber;
+    @OneToMany(mappedBy = "cinema", cascade = CascadeType.ALL)
+    private List<CinemaHallEntity> cinemaHalls;
+}
---
+++ src/main/java/com/example/movieticketbooking/entity/CinemaHallSeatEntity.java
@@ -0,0 +1,28 @@
+package com.example.movieticketbooking.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "cinema_hall_seat")
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class CinemaHallSeatEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    @Column(name = "seat_row", nullable = false)
+    private Character seatRow;
+    @Column(name = "seat_column", nullable = false)
+    private Integer seatCol;
+    @Column(nullable = false)
+    private String type;
+    @ManyToOne(cascade = {CascadeType.DETACH, CascadeType.REFRESH})
+    @JoinColumn(name = "cinema_hall_id")
+    private CinemaHallEntity cinemaHall;
+}
