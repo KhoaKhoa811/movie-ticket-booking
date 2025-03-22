feef13619e7511c00f3fd0cdb1c036eb64c2007f
KhoaKhoa811 <nguyenvoanhkhoa9487@gmail.com>
2025-03-22 23:25:54 +0700

    initialize and adding global exception handler

---
+++ src/main/java/com/example/movieticketbooking/exception/GlobalExceptionHandler.java
@@ -0,0 +1,20 @@
+package com.example.movieticketbooking.exception;
+
+import com.example.movieticketbooking.dto.api.ApiResponse;
+import com.example.movieticketbooking.enums.ErrorCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(value = Exception.class)
+    public ResponseEntity<ApiResponse<?>> handlingException(Exception exception) {
+        ApiResponse<?> exceptionResponse = ApiResponse.builder()
+                .code(ErrorCode.UNCATEGORIZED_EXCEPTION.getCode())
+                .message(ErrorCode.UNCATEGORIZED_EXCEPTION.getMessage())
+                .build();
+        return ResponseEntity.badRequest().body(exceptionResponse);
+    }
+}
